8b8af6a546d7e1492b77ab20c8269788f3de2626
Chris Kottom <chris@chriskottom.com>
2011-09-22 10:52:16 +0200

    basic test cases for TgfReader

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,26 @@ module TestHelper
     assert_equal(value, target_node.state)
   end
 
+  def tgf_reader(input=tgf_content)
+    @tgf_reader ||= Logicle::TgfReader.new(input)
+  end
+
+  def tgf_content
+    @tgf_content ||= <<-TGF.gsub(/^\s+/, "")
+      1 NOT
+      2 AND
+      3 OR
+      4 SWITCH
+      5 SWITCH
+      6 SWITCH
+      7 BULB
+      #
+      4 1
+      5 2
+      6 2
+      2 3
+      1 3
+      3 7
+      TGF
+  end
 end
--- test/tgf_parser_test.rb
+++ test/tgf_parser_test.rb
@@ -6,5 +6,34 @@ require "test/unit"
 class TgfParserTest < Test::Unit::TestCase
   include TestHelper
 
-  
+  def test_initialization_with_filename
+    filename = File.join(File.dirname(__FILE__), "simple.tgf")
+    file_content = File.readlines(filename)
+
+    assert_equal(file_content,
+                 tgf_reader(filename).instance_variable_get(:@contents))
+  end
+
+  def test_initialization_with_string
+    assert_equal(tgf_content.lines.to_a,
+                 tgf_reader.instance_variable_get(:@contents).to_a)
+  end
+
+  def test_parsing
+    circuit = tgf_reader.parse
+  end
+
+  def test_parsing_with_bad_node_type
+    content = "0 FOO\n" << tgf_content
+    assert_raises(Logicle::ParseError) do
+      tgf_reader(content).parse
+    end
+  end
+
+  def test_parsing_with_bad_edge
+    content = tgf_content << "\n10000 100001"
+    assert_raises(Logicle::ParseError) do
+      tgf_reader(content).parse
+    end    
+  end
 end

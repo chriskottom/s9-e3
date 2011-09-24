dd2677b431dc16798538011bb0c541d9a864470c
Chris Kottom <chris@chriskottom.com>
2011-09-24 08:44:07 +0200

    refactoring interface for Simulator

--- lib/logicle/simulator.rb
+++ lib/logicle/simulator.rb
@@ -2,17 +2,19 @@ module Logicle
   class Simulator
     attr_reader  :circuit
 
+    def initialize(*circuits)
+      @circuit = load(circuits.shift)
+      unless circuits.empty?
+        @sub_circuits = circuits.map { |circuit_file| load(circuit_file) }
+      end
+    end
+
     def inputs
       @circuit.inputs
     end
 
     def outputs
       @circuit.outputs
-    end
-
-    def load(circuit_file)
-      reader = TgfReader.new(circuit_file)
-      @circuit = reader.parse
     end
 
     def evaluate
@@ -22,6 +24,12 @@ module Logicle
     def save(output_file)
       writer = TgfWriter.new(output_file, @circuit)
       writer.write
+    end
+
+    private
+    def load(circuit_file)
+      reader = TgfReader.new(circuit_file)
+      @circuit = reader.parse
     end
   end
 end

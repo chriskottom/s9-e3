fd00518d084561add4918b4ab7a3c148c041b7c8
Chris Kottom <chris@chriskottom.com>
2011-09-24 08:12:23 +0200

    moved contents of errors.rb file to lib/logicle.rb

--- lib/logicle.rb
+++ lib/logicle.rb
@@ -1,5 +1,4 @@
 require_relative "logicle/version"
-require_relative "logicle/errors"
 require_relative "logicle/logicle"
 require_relative "logicle/simulator"
 require_relative "logicle/tgf_reader"
@@ -7,3 +6,15 @@ require_relative "logicle/tgf_writer"
 
 require_relative "logicle/node"
 require_relative "logicle/digraph"
+
+module Logicle
+
+  # Raised when trying to create an edge in the graph to or from an unknown node
+  class UnknownNodeError < StandardError; end
+
+  # Raised in case of error while reading an input file 
+  class ParseError < StandardError; end
+
+  # Raised when trying to create a node with an unknown type.
+  class UnknownNodeTypeError < StandardError; end
+end
